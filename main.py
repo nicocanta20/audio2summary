2333583686a9d9af3846fb44e492d0a3385669d5
nicocanta20 <nicocanta20@gmail.com>
2023-08-14 20:43:35 -0300

    Update main.py
    support multiple files

--- main.py
+++ main.py
@@ -1,79 +1,88 @@
 import streamlit as st
 import openai
 from pydub import AudioSegment
+import io
 
-def ogg2mp3(file):
-    sound = AudioSegment.from_ogg(file)
-    sound.export("audio.mp3", format="mp3")
-    return "audio.mp3"
+def process_file(file):
+    byte_io = io.BytesIO(file.read())
+    format_type = file.type.split("/")[1]  # Extracted format here
+    if format_type == "ogg":
+        sound = AudioSegment.from_ogg(byte_io)
+    elif format_type == "wav":
+        sound = AudioSegment.from_wav(byte_io)
+    elif format_type in ["mp3", "mpeg"]:
+        sound = AudioSegment.from_mp3(byte_io)
+    else:
+        raise ValueError(f"Unsupported format: {format_type}")
+    return sound
 
-def wav2mp3(file):
-    sound = AudioSegment.from_wav(file)
-    sound.export("audio.mp3", format="mp3")
-    return "audio.mp3"
+def join_audios(audios):
+    combined = AudioSegment.empty()
+    for audio in audios:
+        combined += audio
+    return combined
 
-def returnSameFormat(file):
-    sound = AudioSegment.from_mp3(file)
-    sound.export("audio.mp3", format="mp3")
-    return "audio.mp3"
-
+def save_audio_to_file(audio, format="mp3"):
+    temp_filename = "temp_audio_file." + format
+    audio.export(temp_filename, format=format)
+    return temp_filename
 
 def audio2text(audio_file):
-    # read the audio file
-    audio_file= open(audio_file, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    res= transcript['text']
-    return res
+    return transcript['text']
 
 def summary(text,language):
     response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo-16k",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant and must make a summary of the following conversation. Must return a bullet point list of the main points of the conversation. Additionaly return the 3 most important ideas of the conversation."},
-        {"role": "user", "content": "respond everything in "+ language},
-        {"role": "user", "content": text},
-    ])
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant. Summarize the following conversation into bullet points."},
+            {"role": "user", "content": "respond everything in " + language},
+            {"role": "user", "content": text},
+        ])
     return response.choices[0]["message"]["content"]
 
-
 def warning_api_key(openai_api_key):
-     if not openai_api_key:
+    if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
 def main():
     st.title("Audio2Summary")
     st.write("### Powered by OpenAI's [Whisper](https://platform.openai.com/docs/guides/speech-to-text) and [GPT-3.5 Turbo](https://platform.openai.com/docs/guides/gpt)")
-    st.write('##### By [Nicolas Cantarovici](https://github.com/nicocanta20) and [Florian Reyes](https://github.com/florianreyes)')
     with st.sidebar:
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-        "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+        st.write("[Get an OpenAI API key](https://platform.openai.com/account/api-keys)")
+        st.divider()
+        st.write('#### By: \n - [Nicolas Cantarovici](https://github.com/nicocanta20) \n - [Florian Reyes](https://github.com/florianreyes)')
     
+    audio_files = st.file_uploader("Upload Audio(s)", type=["ogg", "wav", "mp3", "mpeg"], accept_multiple_files=True)
+    language = st.selectbox("Language", ["English", "Spanish"])
 
-    audio_file = st.file_uploader("Upload Audio", type=["ogg", "wav", "mp3"])
-    language = st.selectbox("Language",   ["English", "Spanish"])
     if st.button("Start", key="whisper"):
-        if audio_file:
+        if audio_files:
             try:
-                if audio_file.type == "audio/ogg":
-                    audio_file = ogg2mp3(audio_file)
-                elif audio_file.type == "audio/wav":
-                    audio_file = wav2mp3(audio_file)
-                else:
-                    audio_file = returnSameFormat(audio_file)
-                    
+                audios = []
+                for audio_file in audio_files:
+                    audios.append(process_file(audio_file))
+
+                combined_audio = join_audios(audios)
+                combined_audio_path = save_audio_to_file(combined_audio)
+
                 warning_api_key(openai_api_key)
                 openai.api_key = openai_api_key
-                text = audio2text(audio_file)
-                response = summary(text,language)
+
+                with open(combined_audio_path, "rb") as f:
+                    text = audio2text(f)
+
+                response = summary(text, language)
+                
                 if response:
                     st.subheader("Summary of the audio file:")
                     st.write(response)
                     with st.expander("Show complete transcript of the audio file"):
                         st.write(text)
-
-            except:
-                st.error("Something went wrong. Please try again.", icon="🚨")
+            except Exception as e:
+                st.error(f"Something went wrong. Please try again. Error: {e}")
 
 if __name__ == "__main__":
     main()
